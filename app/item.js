da255997410e1891b683a9e6b968628e28a2d54f
faroukshouba <faroukshouba@gmail.com>
2015-02-08 22:02:25 +0200

    edita

--- app/item.js
+++ app/item.js
@@ -6,6 +6,10 @@ var mysqlMgr = require('./mysql').mysqlMgr,
 exports.itemMgr = {
 /* Add item */
   addItem : function(body,cb){
+    console.log(body);
+    if(true){
+      console.log("hello");
+    }
     mysqlMgr.connect(function (conn) {
       conn.query('INSERT INTO `item` SET ?',  body,  function(err, result) {
         conn.release();
@@ -17,7 +21,23 @@ exports.itemMgr = {
 
       });
     });
+  },
+  /* get item */
+  getitem : function(cb){
+    mysqlMgr.connect(function (conn) {
+      conn.query('SELECT * FROM `item_type` where deleted = 1', function(err, result) {
+        conn.release();
+        if(err) {
+          util.log(err);
+        } else {
+          console.log(result);
+          cb(result);
+        }
+      });
+    });
   }
+
+
 
 }
 
