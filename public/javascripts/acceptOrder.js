09eeb2ed13d9f4627a25c300f0d125ad47e6c390
abdullah Elamir <abdullah.el_ameer@yahoo.com>
2015-02-23 09:55:28 +0200

    new commit

--- public/javascripts/acceptOrder.js
+++ public/javascripts/acceptOrder.js
@@ -1,9 +1,18 @@
 $(document).ready(function(){
 
-	
- $(function () {
-    $('#myTab a:last').tab('show')
+	 $('body').on('click', '#delete', function () {
+	  console.log($(this).val());
+    $('#confdelete').val($(this).val());
+  });
+  
+  $('body').on('click', '#confdelete', function () {
+    
+
+   
+   // $.get('/order/delete/'+$(this).val(),function(result){
+      //window.location.href='/order/showOrder';
   })
+  });
 
 
 
--- routes/order.js
+++ routes/order.js
@@ -1,108 +1,116 @@
-var express = require('express');
-var orderMgr = require('../app/order').orderMgr;
-var departmentMgr = require('../app/department').departmentMgr;
+	var express = require('express');
+	var orderMgr = require('../app/order').orderMgr;
+	var departmentMgr = require('../app/department').departmentMgr;
 
-var router = express.Router();
-
-
+	var router = express.Router();
 
 
 
-router.get('/getitem/:ido', function(req,res){
-	orderMgr.getitem(req.params.ido,function(result){ 
-		res.send(result);
 
-	});
- 
-});
 
-router.get('/things', function(req,res){
-     res.render('things',{title: 'أضافة موظف'});
+	router.get('/getitem/:ido', function(req,res){
+		orderMgr.getitem(req.params.ido,function(result){ 
+			res.send(result);
 
+		});
+	 
 	});
 
-router.get('/acceptOrder', function(req,res){
-     res.render('acceptOrder',{title: 'acceptOrder'});
+	router.get('/things', function(req,res){
+	     res.render('things',{title: 'أضافة موظف'});
 
+		});
+
+	router.get('/acceptOrder', function(req,res){
+		orderMgr.getUnAcceptedOrder(function(accepted){
+		//console.log(accepted);
+	     res.render('acceptOrder',{title: 'acceptOrder',accep:accepted});
+          });
+		});
+
+
+	router.get('/showOrder', function(req,res){
+	 
+	          orderMgr.getOrder(req.body,function(result){
+	  	      
+	  	         orderMgr.getOrdd(req.body,function(result2,result1){
+	  	         	 departmentMgr.getDepartments(function(result3){
+	  	         	 orderMgr.getOrderInfo(function(resultz,resultw){
+	  	         	 
+	                //console.log(resultw);
+	                //objects = [resultz, resultw]
+	                //console.log([resultz,resultw]);
+	               
+	                //console.log(obj);
+	                // resultz.merge(resultw);
+	                res.render('showOrder',{title: 'أضافة موظف',ord:result,ordd:result2,emp:result3,info1:resultz,info2:resultw});
+	                  
+	  	         	   });
+	                 
+	                 });
+	               });
+	               });
+	             
+	             });
+
+	router.get('/getEmployee/:ido', function(req,res){
+		orderMgr.getEmployee(req.params.ido,function(result){ 
+			res.send(result);
+
+		});
+	 
 	});
 
 
-router.get('/showOrder', function(req,res){
- 
-          orderMgr.getOrder(req.body,function(result){
-  	
-  	         orderMgr.getOrdd(req.body,function(result2){
-  	         	 departmentMgr.getDepartments(function(result3){
-  	         	 orderMgr.getOrderInfo(function(resultz){
-             		console.log(resultz);
-                res.render('showOrder',{title: 'أضافة موظف',ord:result,ordd:result2,emp:result3,orderinfo:resultz});
-                   });
-  	         	   });
-                 
-                 });
-               
-               });
-             
-             });
-
-router.get('/getEmployee/:ido', function(req,res){
-	orderMgr.getEmployee(req.params.ido,function(result){ 
-		res.send(result);
-
+	router.get('/getreminder/:reminder', function(req,res){
+		//console.log("ddd");
+		orderMgr.getreminder(req.params.reminder,function(result3){ 
+			//console.log(result3);
+			res.send(result3);
+			//res.render('AddOrder',{title: 'أضافة موظف',ord3:result3});
+		});
+	 
 	});
- 
-});
 
-
-router.get('/getreminder/:reminder', function(req,res){
-	//console.log("ddd");
-	orderMgr.getreminder(req.params.reminder,function(result3){ 
-		//console.log(result3);
-		res.send(result3);
-		//res.render('AddOrder',{title: 'أضافة موظف',ord3:result3});
+	router.get('/delete/:id', function(req,res){
+	  orderMgr.deleteOrder(req.params.id,function(result){
+	   res.send(result);
+	   });
 	});
- 
-});
-
-router.get('/delete/:id', function(req,res){
-  orderMgr.delete(req.params.id,function(result){
-   res.send(result);
-   });
-});
 
 
-router.post('/saved', function(req,res){
-	//console.log("req.body");
-	//insert this in order table
-	// var discription = req.body.textarea;
-	// var quantity=req.body.quant[0];
-	// var iditeme=req.body.getiteme[0];
-	// var reminder=req.body.reminder;
-	// var requEmploye=req.body.employee;
-	// var confirm=req.body.confirm;
-	// // get employee id by login session
-	// var userEmployeId = 1;
-	// if(confirm==undefined)
-	// {
-	// 	var confirm=0;
-	// }
+	router.post('/saved', function(req,res){
+		//console.log("req.body");
+		//insert this in order table
+		// var discription = req.body.textarea;
+		// var quantity=req.body.quant[0];
+		// var iditeme=req.body.getiteme[0];
+		// var reminder=req.body.reminder;
+		// var requEmploye=req.body.employee;
+		// var confirm=req.body.confirm;
+		// // get employee id by login session
+		// var userEmployeId = 1;
+		// if(confirm==undefined)
+		// {
+		// 	var confirm=0;
+		// }
 
- //    // updaate this in item table 
-	// var iteme_reminder = reminder-quantity; 
-	//console.log(requEmploye);
-	orderMgr.addOrder(req.body,function(result){
-    res.redirect('/order/showOrder');
-    // orderMgr.getOrder(req.body,function(result){
-  	
-  	 //         orderMgr.getOrdd(req.body,function(result2){
-  	 //         	 departmentMgr.getDepartments(req.body,function(result3){
-    //             res.render('addOrder',{title: 'أضافة موظف',ord:result,ordd:result2,emp:result3,ma:true});
-    //                });
-                 
-    //              });
-               
-    //            });
-  });
-});
+	 //    // updaate this in item table 
+		// var iteme_reminder = reminder-quantity; 
+		//console.log(requEmploye);
+		orderMgr.addOrder(req.body,function(result){
+	    res.redirect('/order/showOrder');
+	    // orderMgr.getOrder(req.body,function(result){
+	  	
+	  	 //         orderMgr.getOrdd(req.body,function(result2){
+	  	 //         	 departmentMgr.getDepartments(req.body,function(result3){
+	    //             res.render('addOrder',{title: 'أضافة موظف',ord:result,ordd:result2,emp:result3,ma:true});
+	    //                });
+	                 
+	    //              });
+	               
+	    //            });
+	  });
+	});
 
-module.exports = router;
+	module.exports = router;
